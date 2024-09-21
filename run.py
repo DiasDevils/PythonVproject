ac248adf7213b9c0d489d0e10486d90fbc8a6a16
Dia Ledesma <dia81818181@gmail.com>
2024-09-21 20:27:57 +0000

    attempt at first function to get delivery but it is separate and it is not user friendly. It will need to change

--- run.py
+++ run.py
@@ -13,6 +13,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Vproject')
 
-delivery = SHEET.worksheet('delivery')
-data = delivery.get_all_values()
-print(data)
+""" code to check if getting data is working """
+# delivery = SHEET.worksheet('delivery')
+# data = delivery.get_all_values()
+# print(data)
+
+""" get delivery input from user """
+def get_delivery():
+    print("Please enter vaccine delivery data.")
+    print("Please enter the batch number. For example: 1")
+    d_str1= input("Enter your batch here:")
+
+    print("Please enter the delivery month in date format. For example 31/12/2024")
+    d_str2= input("Enter date here:")
+
+    print("Please enter the vaccine name. Accepts valid vaccines only: flu-one or flu-two")
+    d_str3= input("Enter vaccine name here:")
+
+    print("Please enter the quantity of vials delivered: For example: 20")
+    d_str4= input("Enter quantity here:")
+
+    print(f"The data you entered for delivery is Batch {d_str1}, on the {d_str2}, vaccine name {d_str3}, quantity {d_str4}")
+
+get_delivery()
