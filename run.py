afa708572cc9e04da1640b038d792c09c3db88ec
Dia Ledesma <dia81818181@gmail.com>
2024-10-02 08:58:05 +0000

    edited code as I created a bug by movin a line up

--- run.py
+++ run.py
@@ -301,13 +301,13 @@ def main_menu():
 
     while True:
         print("Please follow instructions for the FVST System below.")
-        choice = input('Please Select Option 1,2,3 or 4 to continue:')
+        print('Menu:')
         print('---------------------')
         print('Option 1.Input Delivery Data')
         print('Option 2.Input Usage Data')
         print('Option 3.View Stock Table')
         print('Option 4.Exit')
-        # choice = input('Please Select Option 1,2,3 or 4 to continue:')
+        choice = input('Please Select Option 1,2,3 or 4 to continue:')
         if choice == '1':
             delivery_data = get_delivery()
             if delivery_data:
